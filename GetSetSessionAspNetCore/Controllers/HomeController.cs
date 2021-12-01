d5a2ee3cb2cbc2dc3212329833a66d5ba7ceac63
Your Name <53593343+CoreProgramm@users.noreply.github.com>
2021-12-01 22:22:43 +0530

    Add project files.

---
+++ GetSetSessionAspNetCore/Controllers/HomeController.cs
@@ -0,0 +1,47 @@
+﻿using GetSetSessionAspNetCore.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GetSetSessionAspNetCore.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            //Set value in Session object.
+            HttpContext.Session.SetString("name", "John Smith");
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Index(string sessionName)
+        {
+            //Get value from Session object.
+            ViewBag.Name = HttpContext.Session.GetString(sessionName);
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
